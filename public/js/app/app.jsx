da9f08e0791e3969dd4b093d5f90e661e6bd2714
Elena Rakhimova <elenara@kth.se>
2023-01-04 15:02:33 +0100

    fix: add error fallback and remove admin context

--- public/js/app/app.jsx
+++ public/js/app/app.jsx
@@ -9,6 +9,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { WebContextProvider } from './context/WebContext'
 import { uncompressData } from './context/compress'
 import { AdminContextProvider } from './context/AdminContext'
+import { ErrorBoundary } from 'react-error-boundary'
 
 import '../../css/node-web.scss'
 
@@ -18,6 +19,16 @@ import Simple from './pages/Simple'
 
 export default appFactory
 
+function ErrorFallback({ error, resetErrorBoundary }) {
+  console.error(error)
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
+}
 _renderOnClientSide()
 
 function _renderOnClientSide() {
@@ -29,32 +40,30 @@ function _renderOnClientSide() {
   const webContext = {}
   uncompressData(webContext)
 
-  const adminContext = {}
-  uncompressData(adminContext, 'admin')
-
   const basename = webContext.proxyPrefixPath.uri
 
-  const app = <BrowserRouter basename={basename}>{appFactory({}, webContext, adminContext)}</BrowserRouter>
-
+  const app = (
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => {
+        location.reload()
+        // reset the state of your app so the error doesn't happen again
+      }}
+    >
+      <BrowserRouter basename={basename}>{appFactory({}, webContext)}</BrowserRouter>{' '}
+    </ErrorBoundary>
+  )
   const domElement = document.getElementById('app')
   hydrateRoot(domElement, app)
 }
 
-function appFactory(applicationStore, context, adminContext) {
+function appFactory(applicationStore, context) {
   return (
     <WebContextProvider configIn={context}>
       <Routes>
-        <Route path="/" element={<Start />} />
-        <Route path="/secure" element={<Simple headingId="template_secured_page_heading" />} />
-        <Route path="/silent" element={<Simple headingId="template_silent_login_page_heading" />} />
-        <Route
-          path="/_admin"
-          element={
-            <AdminContextProvider configIn={adminContext}>
-              <AdminStart />
-            </AdminContextProvider>
-          }
-        />
+        <Route exact path="/" element={<Start />} />
+        <Route exact path="/node" element={<Start />} />
+
         <Route path="*" element={<Simple headingId="template_not_found_page_heading" />} />
       </Routes>
     </WebContextProvider>
