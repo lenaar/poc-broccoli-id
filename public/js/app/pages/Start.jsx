b8a2d5663abe448bec074f3369b8b47312848bdc
Elena Rakhimova <elenara@kth.se>
2023-01-04 15:09:38 +0100

    feat: add start page for bank id prototype

--- public/js/app/pages/Start.jsx
+++ public/js/app/pages/Start.jsx
@@ -1,42 +1,80 @@
 import React from 'react'
 
+import Button from '../components/Button'
+import Results from '../components/Results'
 import { useWebContext } from '../context/WebContext'
 
-import i18n from '../../../../i18n'
+import { STATUS, ERROR_ASYNC, useQrCodeAsync } from '../hooks/qrCodeUseAsync'
+import { useAuthAsync } from '../hooks/authUseAsync'
+import { useCollectAsync } from '../hooks/collectUseAsync'
 
-import Button from '../components/Button'
+function QrCodeResults({ responseStatus, error = {}, data }) {
+  const [prn, setPrm] = React.useState('')
+
+  const { data: authData, status: authStatus, error: authError } = useAuthAsync({ prn }, 'onChange')
+  const { message: authMessage = '', orderRef = '' } = authData || {}
+
+  const { data: collectData, status: collectStatus, error: collectError } = useCollectAsync({ orderRef }, 'onChange')
+  console.log('prn', prn)
+  console.log('authData', authData)
+  console.log('authStatus', authStatus)
+  console.log('authError', authError)
+
+  console.log('collectData', collectData)
+  console.log('collectStatus', collectStatus)
+  console.log('collectError', collectError)
+
+  return (
+    <Results responseStatus={responseStatus} error={error}>
+      <h3>Visa resultat</h3>
+      {responseStatus === 'resolved' && (
+        <>
+          <p>{data.qrCode}</p>
+          <input type="text" value={prn} name="personal-number" onChange={e => setPrm(e.target.value)} />
+          {authStatus === 'resolved' && (
+            <>
+              <p>{authMessage}</p>
+              <p>OrderRef: {orderRef}</p>
+            </>
+          )}
+
+          {collectStatus === 'resolved' && (
+            <>
+              <h3>Collection data is finished</h3>
+              <p>Message{collectData.message}</p>
+              <p>Status: {collectData.status}</p>
+              <p>completionData: {collectData.completionData}</p>
+            </>
+          )}
+        </>
+      )}
+    </Results>
+  )
+}
+
+function QrCodeArea({ showQR }) {
+  const state = useQrCodeAsync({ showQR }, 'onChange')
+  console.log('state in QrCodeArea', state)
+
+  const { data, status: responseStatus, error = {} } = state || {}
+  return <QrCodeResults responseStatus={responseStatus} error={error} data={data} />
+}
 
 const Start = () => {
-  const [webContext, setContext] = useWebContext()
-  const { message, lang, count = 0 } = webContext
-
-  const incrementCount = () => {
-    const newVal = { count: count + 1 }
-    setContext({ ...webContext, ...newVal })
-  }
+  const [webContext] = useWebContext()
+  const { lang } = webContext
+  const [showQR, setShowQR] = React.useState(false)
 
   return (
     <main id="mainContent">
-      <h1>Node-web</h1>
-      <h2>{i18n.message('template_app_works', lang)}</h2>
-      <hr className="my-2" />
-      <p>{`${i18n.message('template_store_text', lang)}: ${message}`}</p>
-
-      <Button caption={i18n.message('template_try_me', lang)} lang={lang} />
+      <h1>Aktivera och använda mobilt brocolliid</h1>
       <hr />
       <Button
-        caption={`${i18n.message('template_button_increment', lang)} ${count}`}
+        caption={`${showQR ? 'Stänga' : 'Använda'} Mobilt broccoliid`}
         lang={lang}
-        onClick={incrementCount}
+        onClick={() => setShowQR(!showQR)}
       />
-      <hr />
-      <div>
-        <a href="/node/secure">{i18n.message('template_secured_page_heading', lang)}</a>
-        <br />
-        <a href="/node/_admin">{i18n.message('template_secured_admin_page_heading', lang)}</a>
-        <br />
-        <a href="/node/silent">{i18n.message('template_silent_login_page_heading', lang)}</a>
-      </div>
+      <QrCodeArea showQR={showQR} />
     </main>
   )
 }
