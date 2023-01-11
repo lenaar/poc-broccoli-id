91a1e4528dcf882133903abb540a44ffac7f68a4
Elena Rakhimova <elenara@kth.se>
2023-01-11 16:42:11 +0100

    feat: generate qr code and prepare it to future dynamic qr

--- server/controllers/bankIdCtrl.js
+++ server/controllers/bankIdCtrl.js
@@ -1,11 +1,12 @@
 const axios = require('axios')
+const crypto = require('crypto')
 const https = require('https')
 const fs = require('fs')
 const path = require('path')
 const log = require('@kth/log')
 
-const bankIdApiUrl = 'https://appapi2.test.bankid.com/rp/v5'
-
+const bankIdApiUrl = 'https://appapi2.test.bankid.com/rp/v5.1'
+// QR CODES https://appapi2.bankid.com/rp/v5.1
 function selectFile(filename, type) {
   return fs.readFileSync(path.resolve(__dirname, '../../cert', filename), type)
 }
@@ -31,24 +32,51 @@ function getBrocolliIdAgent() {
   }
 }
 
+let QR_START_SECRET_CACHE = ''
+let ORDER_TIME = ''
+
+function generateQrCode(authData, orderTime = ORDER_TIME) {
+  const { qrStartToken, qrStartSecret = QR_START_SECRET_CACHE, orderRef = '' } = authData || {}
+  const qrTime = new Date()
+  const qrTimeSeconds = new Date(qrTime - orderTime).getSeconds()
+  const qrAuthCode = crypto.createHmac('sha256', qrStartSecret).update(qrTimeSeconds.toString()).digest('hex')
+  const qrCodeStr = `bankid.${qrStartToken}.${qrTimeSeconds}.${qrAuthCode}`
+  return { qrCodeStr, qrAuthCode, qrTimeSeconds }
+}
+
 async function authBroccolliId(req, res, next) {
   const { params, query } = req
   log.info(` trying to auth order `, { params, query })
-  log.info(req.connection.remoteAddress)
+  log.info(' connection remote addres', req.connection.remoteAddress)
   // data.data.orderRef
   const { data } = await axios.create(getBrocolliIdAgent()).post(
     bankIdApiUrl + '/auth',
     JSON.stringify({
-      personalNumber: params.pnr, // req.query.pnr
-      endUserIp: '127.0.0.1',
+      // personalNumber: params.pnr, // req.query.pnr
+      endUserIp: '192.168.1.217',
       // endUserIp: '10.56.40.158', // TODO: must be client ip as seen been by RP
     })
   )
-
+  const orderTime = new Date()
+  const { qrStartToken, qrStartSecret } = data
+  QR_START_SECRET_CACHE = qrStartSecret
+  ORDER_TIME = orderTime
+  const { qrCodeStr } = generateQrCode(data, orderTime)
+  // qrStartSecret must not be sent to the client
+  // bankid is a fixed prefix.
+  // qrStartToken is from the auth or sign response.
+  // time is the number of seconds since the result from auth or sign was returned.
+  // qrAuthCode is computed as HMACSHA256(qrStartSecret, time)
   log.info('data', { data })
   log.info('auth initiated, please open bankid to authenticate ', { orderRef: data.orderRef })
 
-  res.json({ message: 'auth initiated, please open bankid to authenticate ', orderRef: data.orderRef }) // orderRef
+  res.json({
+    message: 'auth initiated, please open bankid to authenticate ',
+    orderRef: data.orderRef,
+    orderTime,
+    qrStartToken,
+    qrCodeStr,
+  }) // orderRef
 }
 
 function timeout(ms) {
